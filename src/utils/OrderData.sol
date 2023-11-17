6d472c1274373e2ebe58c5c46f1869d08294c29c
TheoLeFur <theo.lefur@epfl.ch>
2023-11-17 23:58:10 +0300

    libraries

---
+++ src/utils/OrderData.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+library OrderData { 
+    struct Order {
+        uint256 jsonHash;
+        uint256 expirationTimestamp; 
+        SolverData solverData;
+    }
+
+    struct SolverData { 
+        uint256 stakeAmount; 
+        
+    }
+}
